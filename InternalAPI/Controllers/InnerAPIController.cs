04f031088cfd1ea7d222e9bed04727d0dc3bff82
Kamal Mehra <Kamal.Mehra@welkgroup.com>
2020-04-22 16:03:52 +0530

    Add project files.

---
+++ InternalAPI/Controllers/InnerAPIController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace InternalAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InnerAPIController : ControllerBase
+    {
+
+        private DateTime _requestTime;
+        private DateTime responseTime;
+        private bool flag=false;
+        List<InnerAPI> innerAPI = new List<InnerAPI>();
+        private string _apiBaseURI = "https://localhost:44319/weatherforecast";
+       
+
+        // GET: api/InnerAPI
+        //[Route("api/InnerAPI")]
+        [HttpGet]
+        public List<InnerAPI> Get()             
+        
+        
+        {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(_apiBaseURI);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                _requestTime = DateTime.Now;
+                HttpResponseMessage responseMessage = client.GetAsync("WeatherForecast").Result;
+                responseTime = DateTime.Now;
+
+                if (responseMessage.IsSuccessStatusCode)
+                {
+
+                    innerAPI = JsonConvert.DeserializeObject<List<InnerAPI>>
+                    (responseMessage.Content.ReadAsStringAsync().Result); //resapi
+                    flag = true;
+                }
+                else
+                {
+                    flag = false;
+                }
+                TimeSpan letancy = responseTime - _requestTime;
+                string status = flag.Equals("true") ? "Fail" : "Success";
+
+                LoggerExtensions.LogSoapApiResponseTime(nameof(Get), _requestTime, responseTime,status, "",
+                letancy, "", "", "");
+
+                return innerAPI;
+            }
+            catch(Exception ex)
+            {
+                TimeSpan letancy = responseTime - _requestTime;
+               
+                var st = new StackTrace(ex, true);
+                var line = st.GetFrame(0).GetFileLineNumber();
+                
+                LoggerExtensions.LogSoapApiResponseTime(nameof(Get), _requestTime, responseTime, "Fail", ex.ToString(),
+                letancy, ex.Message.ToString(), ex.InnerException.ToString(), ex.StackTrace.ToString());
+
+                 throw;
+            }
+            
+        }
+
+       
+    }
+}
---
+++ InternalAPI/LoggerExtensions.cs
@@ -0,0 +1,91 @@
+﻿using CsvHelper;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace InternalAPI
+{
+    public static class LoggerExtensions
+    {
+
+
+        public class loggercsv
+        {
+            public string Methodname { get; set; }
+            public DateTime RequestTime { get; set; }
+            public DateTime ResponseTime { get; set; }
+            public string Status { get; set; }
+            public string Argument { get; set; }
+            public TimeSpan Letancy { get; set; }
+            public string Exception { get; set; }
+            public string ErrorMessage { get; set; }
+            public string InnerException { get; set; }
+            public string StckTrace { get; set; }
+
+        }
+
+        
+
+        public static void LogSoapApiResponseTime(string methodname,DateTime requestTime, DateTime 
+            responseTime, string status, string exception, TimeSpan letancy,string errormessage,string innerexception,
+            string errortrace)
+        {
+
+            try
+            {
+
+                var record = new List<loggercsv>
+                {
+                    new loggercsv
+                    {
+                        Methodname=methodname,
+                        RequestTime=requestTime,
+                        ResponseTime=responseTime,
+                        Status=status,
+                        Argument="{}",
+                        Letancy=letancy,
+                        Exception=exception,
+                        ErrorMessage=errormessage,
+                        InnerException=innerexception,
+                        StckTrace=errortrace,
+                    }
+                };
+                string path = @"C:\Users\ATEAM\source\repos\ExtrnalAPI\InternalAPI\Logger.csv";
+
+                if(File.Exists(path))
+                {
+                    using (StreamWriter file = File.AppendText(path))
+                    using (var csv = new CsvWriter(file, CultureInfo.InvariantCulture))
+                    {
+                        csv.Configuration.HasHeaderRecord = false;
+                        csv.WriteRecords(record);
+                    }
+                }
+                else
+                {
+                    using (StreamWriter file = File.CreateText(path))
+                    using (var csv = new CsvWriter(file, CultureInfo.InvariantCulture))
+                    {
+                       
+                        csv.WriteRecords(record);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+      
+    }
+}
